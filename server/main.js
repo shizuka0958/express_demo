a21678eb81c36f9c3e606f115f404056b15f0c2f
zhangming <shizuka@mocait.com>
2018-05-11 10:02:19 +0800

    add cors

--- server/main.js
+++ server/main.js
@@ -9,12 +9,33 @@ var httpServer = require('http').Server(app);
 var path = require('path');
 var ip = require('ip');
 var moment = require('moment');
+var cors = require('cors')
 
 var db = require('./db/');
 
 const httpport = process.env.PORT || 8080;
 
+// method 1：
+// var allowCrossDomain = function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');//自定义中间件，设置跨域需要的响应头。
+//     next();
+//    };
 
+// app.use(allowCrossDomain);//运用跨域的中间件
+
+// method 2:
+// app.all('*', function (req, res, next) {
+//     res.header("Access-Control-Allow-Credentials", true)
+//     res.header("Access-Control-Allow-Origin", "*")
+//     res.header("Access-Control-Allow-Headers", "X-Requested-With")
+//     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
+//     res.header("X-Powered-By", ' 3.2.1')
+//     res.header("Content-Type", "application/json;charset=utf-8")
+//     next()
+// })
+
+// method 3: use cors
+app.use(cors())
 app.use(require('express').static(path.join(__dirname, 'public')));
 
 
@@ -24,31 +45,25 @@ app.get('/test', function (req, res) {
 
     var time_arry = new Array();
 
-        db.getConnection().query("select * from face", function (error, results, fields) {
+    db.getConnection().query("select * from face", function (error, results, fields) {
         if (error) {
             throw error;
         }
         if (results) {
-           
-            for (var i = 0; i < results.length; i++)     {
-            
+
+            for (var i = 0; i < results.length; i++) {
+
                 var doc = {};
                 doc.time = moment(results[i].entry_time).format("YYYY-MM-DD HH:mm:ss");
                 console.log(doc.time);
                 time_arry.push(doc);
-                console.log(time_arry.length); 
+                console.log(time_arry.length);
             }
         }
         res.send(time_arry);
-    });   
+    });
 });
 
-
-// app.get('*', function (req, res) {
-//     res.set('Content-Type', 'application/json');
-//     var ret = test_mysql();
-//     res.send(ret);
-// });
 
 httpServer.listen(httpport, function () {
     console.log('listening on ' + ip.address() + ":" + httpport);
