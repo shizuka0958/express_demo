65e8cdfb83285c7668589befc39cb65639a5304a
zhangming <shizuka@mocait.com>
2018-05-15 10:21:21 +0800

    1.add totalcount 2.modify throw error 3.modify res_obj

--- server/main.js
+++ server/main.js
@@ -74,8 +74,6 @@ app.get('/getPersonTimes', function (req, res) {
         sqlStr = sqlStr + 'time <= ' + "'" + req.query.endTime + "'" + ' and ';
     }
 
-    console.log(sqlStr);
-
     var hasFilter = false;
     for (key in req.query) {
         if ('' != req.query[key]) {
@@ -92,22 +90,18 @@ app.get('/getPersonTimes', function (req, res) {
 
     console.log(sqlStr);
 
-    //var connection = db.createConnection('test');
-
     var p1 = new Promise(function (resolve, reject) {
         pool.query('select CamID from face group by CamID', function (error, results, fields) {
             if (error) {
-                throw error;
+                reject(error);
             }
             if (results) {
                 var array1 = new Array();
                 for (var i = 0; i < results.length; i++) {
-
-                    console.log(results[i].CamID);
                     array1.push(results[i].CamID);
                 }
+                resolve(array1);
             }
-            resolve(array1);
         });
 
     });
@@ -115,20 +109,21 @@ app.get('/getPersonTimes', function (req, res) {
     var p2 = new Promise(function (resolve, reject) {
         pool.query(sqlStr, function (error, results, fields) {
             if (error) {
-                throw error;
+                reject(error);
             }
             if (results) {
                 var array2 = new Array();
                 for (var i = 0; i < results.length; i++) {
-    
+
                     var doc = {};
                     doc.camID = results[i].CamID;
                     doc.count = results[i].count;
                     array2.push(doc);
-    
+
                 }
+                resolve(array2);
             }
-            resolve(array2);
+
         });
     });
 
@@ -136,35 +131,46 @@ app.get('/getPersonTimes', function (req, res) {
     // 同时执行p1和p2，并在它们都完成后执行then:
     Promise.all([p1, p2]).then(function (results) {
         console.log(results); // 获得一个Array: ['P1', 'P2']
+        var res_obj = {};
+        res_obj.code = 0;
+        res_obj.msg = '';
+        var data = {};
 
-        var res_array = new Array();
-        for(var i =0;i < results[0].length;i++){
+        var list = [];
+        for (var i = 0; i < results[0].length; i++) {
             var doc = {};
             doc.camID = results[0][i];
             var isZero = true;
             var count = 0;
-            for(var j =0;j < results[1].length;j++){
-                console.log('results[0][i] = '+results[0][i]);
-                console.log('results[1][j] = '+results[1][j]);
-                console.log('results[1][j].CamID = '+results[1][j].camID);
-                if(results[0][i] === results[1][j].camID){
+            for (var j = 0; j < results[1].length; j++) {
+                console.log('results[0][i] = ' + results[0][i]);
+                console.log('results[1][j] = ' + results[1][j]);
+                console.log('results[1][j].CamID = ' + results[1][j].camID);
+                if (results[0][i] === results[1][j].camID) {
                     isZero = false;
                     count = results[1][j].count;
-                    console.log('---break---');
                     break;
                 }
             }
 
-            if(isZero){
+            if (isZero) {
                 doc.count = 0;
-            }else{
+            } else {
                 doc.count = count;
             }
-           
-            res_array.push(doc);
+
+            list.push(doc);
         }
-        console.log(res_array);
-        res.send(res_array);
+        data.list = list;
+        res_obj.data = data;
+        res.send(res_obj);
+
+    }).catch(function (error) {
+        var res_obj = {};
+        res_obj.code = -1;
+        res_obj.msg = '数据库查询失败!'
+        res_obj.data = {};
+        res.send(res_obj);
     });
 
     //db.disconnect(connection);
@@ -176,76 +182,110 @@ app.get('/getDetailData', function (req, res) {
     console.log(JSON.stringify(req.query));
 
     var sqlStr = 'select CamID,Pic,Sex,Age,time,TopID,TopName from face where ';
+    var sqlStr_count = 'select count(*) as count from face where '
 
+    var sqlStrFilter = '';
     if (req.query.hasOwnProperty('camID') && ('' != req.query.camID)) {
-        sqlStr = sqlStr + 'CamID =' + "'" + req.query.camID + "'" + ' and ';
+        sqlStrFilter = sqlStrFilter + 'CamID =' + "'" + req.query.camID + "'" + ' and ';
     }
     if (req.query.hasOwnProperty('sex') && ('' != req.query.sex)) {
-        sqlStr = sqlStr + 'Sex =' + "'" + req.query.sex + "'" + ' and ';
+        sqlStrFilter = sqlStrFilter + 'Sex =' + "'" + req.query.sex + "'" + ' and ';
     }
     if (req.query.hasOwnProperty('startAge') && ('' != req.query.startAge)) {
-        sqlStr = sqlStr + 'Age >' + "'" + req.query.startAge + "'" + ' and ';
+        sqlStrFilter = sqlStrFilter + 'Age >' + "'" + req.query.startAge + "'" + ' and ';
     }
     if (req.query.hasOwnProperty('endAge') && ('' != req.query.endAge)) {
-        sqlStr = sqlStr + 'Age <' + "'" + req.query.endAge + "'" + ' and ';
+        sqlStrFilter = sqlStrFilter + 'Age <' + "'" + req.query.endAge + "'" + ' and ';
     }
     if (req.query.hasOwnProperty('startTime') && ('' != req.query.startTime)) {
-        sqlStr = sqlStr + 'time >= ' + "'" + req.query.startTime + "'" + ' and ';
+        sqlStrFilter = sqlStrFilter + 'time >= ' + "'" + req.query.startTime + "'" + ' and ';
     }
     if (req.query.hasOwnProperty('endTime') && ('' != req.query.endTime)) {
-        sqlStr = sqlStr + 'time <= ' + "'" + req.query.endTime + "'" + ' and ';
+        sqlStrFilter = sqlStrFilter + 'time <= ' + "'" + req.query.endTime + "'" + ' and ';
     }
 
-    console.log(sqlStr);
     var hasFilter = false;
     for (key in req.query) {
         if ('' != req.query[key]) {
-            sqlStr = sqlStr.slice(0, -4); //remove and
+            sqlStrFilter = sqlStrFilter.slice(0, -4); //remove and
             hasFilter = true;
             break;
         }
     }
     if (!hasFilter) {
-        sqlStr = sqlStr.slice(0, -6);  //remove where
+        sqlStrFilter = sqlStrFilter.slice(0, -6);  //remove where
     }
 
 
     //limit 
+    var sqlStrLimit = '';
     if (req.query.hasOwnProperty('limitStartPos') && ('' != req.query.limitStartPos)) {
         if (req.query.hasOwnProperty('limitNumber') && ('' != req.query.limitNumber)) {
-            sqlStr = sqlStr + ' limit ' + req.query.limitStartPos + ' , ' + req.query.limitNumber;
+            sqlStrLimit = sqlStrLimit + ' limit ' + req.query.limitStartPos + ' , ' + req.query.limitNumber;
         } else {
-            sqlStr = sqlStr + ' limit ' + req.query.limitStartPos + ' , ' + '10';
+            sqlStrLimit = sqlStrLimit + ' limit ' + req.query.limitStartPos + ' , ' + '10';
         }
     }
 
+    sqlStr = sqlStr + sqlStrFilter + sqlStrLimit;
+    sqlStr_count = sqlStr_count + sqlStrFilter;
 
     console.log(sqlStr);
+    console.log(sqlStr_count);
 
-    var res_arry = new Array();
-
-    //var connection = db.createConnection('test');
-    pool.query(sqlStr, function (error, results, fields) {
-        if (error) {
-            throw error;
-        }
-        if (results) {
-            for (var i = 0; i < results.length; i++) {
-
-                var doc = {};
-                doc.camID = results[i].CamID;
-                doc.pic = results[i].Pic;
-                doc.sex = results[i].Sex;
-                doc.age = results[i].Age;
-                doc.topID = results[i].TopID;
-                doc.topName = results[i].TopName;
-                doc.time = moment(results[i].time).format("YYYY-MM-DD HH:mm:ss");
-                res_arry.push(doc);
+    var p1 = new Promise(function (resolve, reject) {
+        pool.query(sqlStr_count, function (error, results, fields) {
+            var totalCount = 0;
+            if (error) {
+                reject(error);
             }
-        }
-        res.send(res_arry);
+            if (results) {
+                totalCount = results[0].count;
+                resolve(totalCount);
+            }
+
+        });
     });
-    //db.disconnect(connection);
+
+    p1.then(function (value) {
+        pool.query(sqlStr, function (error, results, fields) {
+            var res_obj = {};
+
+            if (error) {
+                reject(error);
+            }
+            if (results) {
+                res_obj.code = 0;
+                res_obj.msg = '';
+                var data = {};
+
+                var list = [];
+                for (var i = 0; i < results.length; i++) {
+
+                    var doc = {};
+                    doc.camID = results[i].CamID;
+                    doc.pic = results[i].Pic;
+                    doc.sex = results[i].Sex;
+                    doc.age = results[i].Age;
+                    doc.topID = results[i].TopID;
+                    doc.topName = results[i].TopName;
+                    doc.time = moment(results[i].time).format("YYYY-MM-DD HH:mm:ss");
+                    list.push(doc);
+                }
+                data.list = list;
+                data.totalCount = value;
+                res_obj.data = data;
+
+                res.send(res_obj);
+            }
+        });
+    }).catch(function (error) {
+        var res_obj = {};
+        res_obj.code = -1;
+        res_obj.msg = '数据库查询失败!'
+        res_obj.data = {};
+        res.send(res_obj);
+    });
 });
 
 app.get('/getCameraList', function (req, res) {
@@ -254,22 +294,30 @@ app.get('/getCameraList', function (req, res) {
 
     var sqlStr = 'select CamID from face group by CamID';
 
-    var res_arry = new Array();
-
     //var connection = db.createConnection('test');
     pool.query(sqlStr, function (error, results, fields) {
+        var res_obj = {};
         if (error) {
+            res_obj.code = -1;
+            res_obj.msg = '数据库查询失败!'
+            res_obj.data = {};
+            res.send(res_obj);
             throw error;
         }
         if (results) {
+            res_obj.code = 0;
+            res_obj.msg = '';
+            var data = {};
+            var list = [];
 
             for (var i = 0; i < results.length; i++) {
-                res_arry.push(results[i].CamID);
+                list.push(results[i].CamID);
             }
+            data.list = list;
+            res_obj.data = data;
+            res.send(res_obj);
         }
-        res.send(res_arry);
     });
-    //db.disconnect(connection);
 });
 
 httpServer.listen(httpport, function () {
